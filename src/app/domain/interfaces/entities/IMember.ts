81c35dea861051b27157e7e062f52d7bbc80e5f4
Lucas Apoena <lucasapoena@gmail.com>
2022-02-14 20:42:29 -0300

    Members :: Inclusao das propriedades

--- src/app/domain/interfaces/entities/IMember.ts
+++ src/app/domain/interfaces/entities/IMember.ts
@@ -1,4 +1,20 @@
+import { IAddress } from './../valueObjects/IAddreess';
 export interface IMember {
   _id: string;
-  name: string;
+  nome: string;
+  apelido: string;
+  padrinho: string;
+  data_apresentacao: Date;
+  data_nascimento: Date;
+  cpf: string;
+  rg: string;
+  veiculo:string;
+  cnh:string;
+  cnh_categoria:string;
+  tipo_sanguineo:string;
+  telefone:string;
+  telefone_emergencia:string;
+  data_admissao:Date;
+  url_foto: string;
+  endereco: IAddress;
 }
---
+++ src/app/domain/interfaces/valueObjects/IAddreess.ts
@@ -0,0 +1,10 @@
+export interface IAddress{
+  cep: string;
+  tipo: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+}
